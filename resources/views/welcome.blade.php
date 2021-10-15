7e518b059bc2823219e405395344543259749859
AuziMan <austintdriver@gmail.com>
2021-10-15 12:27:00 -0700

    NewPush

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        background: url("/images/backgroundimg.jpg");
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
