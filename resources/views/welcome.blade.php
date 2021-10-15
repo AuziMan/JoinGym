1adc2be6cc9a231622e3312e0f0115d0161a9589
AuziMan <austintdriver@gmail.com>
2021-10-15 12:14:10 -0700

    NewPush

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Austin's Heroku App
                 </div>
 
                 <div class="links">
